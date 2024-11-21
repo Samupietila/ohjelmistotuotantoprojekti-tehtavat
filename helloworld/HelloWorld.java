3aea363fda9c9a6f4bda35cbe1ab13c2360d0ae4
Samu <samupie@metropolia.fi>
2024-11-21 11:13:10 +0200

    ei löytynyt buugia

---
+++ helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * This program says welcome 
+ **/
+
+public class HelloWorld {
+    public static void main(final String[] args) {
+        // this says welcome
+        System.out.println("Welcome to checkstyle.");
+        int x = 10;
+        int y = 0;
+        int result = x / y;
+    }
+        
+}
